8340f5c5a9f15d0407b315f1aa0022df6dd4616b
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2024-12-02 00:28:31 -0300

    feat: client objects abstraction

--- entidade/pessoa.py
+++ entidade/pessoa.py
@@ -3,39 +3,58 @@ from typing import List
 from entidade.carro_classico import CarroClassico
 
 class Pessoa(ABC):
-    @abstractmethod
     def __init__(self, nome: str, documento: str, carros: List[CarroClassico] = None):
-        pass
+        if not isinstance(nome, str):
+            raise TypeError("O nome deve ser uma string")
+        if not isinstance(documento, str):
+            raise TypeError("O documento deve ser uma string")
+        if carros is None:
+            carros = []
+        elif not all(isinstance(carro, CarroClassico) for carro in carros):
+            raise TypeError("Todos os carros devem ser instâncias da classe CarroClassico")
+
+        self._nome = nome
+        self._documento = documento
+        self._carros = carros
 
     @property
-    @abstractmethod
     def nome(self) -> str:
-        pass
-    
+        return self._nome
+
     @nome.setter
-    @abstractmethod
     def nome(self, nome: str):
-        pass
+        if not isinstance(nome, str):
+            raise TypeError("O nome deve ser uma string")
+        self._nome = nome
 
     @property
-    @abstractmethod
-    def carros(self) -> List[CarroClassico]:
-        pass
-  
-    @abstractmethod
-    def add_carro(self, carro: CarroClassico):
-        pass
-
-    @abstractmethod
-    def del_carro(self, carro: CarroClassico):
-        pass
-
-    @property
-    @abstractmethod
     def documento(self) -> str:
-        pass
+        return self._documento
 
     @documento.setter
-    @abstractmethod
     def documento(self, documento: str):
+        if not isinstance(documento, str):
+            raise TypeError("O documento deve ser uma string")
+        self._documento = documento
+
+    @property
+    def carros(self) -> List[CarroClassico]:
+        return self._carros
+
+    def add_carro(self, carro: CarroClassico):
+        if not isinstance(carro, CarroClassico):
+            raise TypeError("O carro deve ser uma instância da classe CarroClassico")
+        if carro in self._carros:
+            raise ValueError("O carro já está na lista")
+        self._carros.append(carro)
+
+    def del_carro(self, carro: CarroClassico):
+        if not isinstance(carro, CarroClassico):
+            raise TypeError("O carro deve ser uma instância da classe CarroClassico")
+        if carro not in self._carros:
+            raise ValueError("O carro não está na lista")
+        self._carros.remove(carro)
+
+    @abstractmethod
+    def tipo(self) -> str:
         pass
