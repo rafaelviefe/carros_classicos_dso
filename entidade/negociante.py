e277e603a2504015dc115a6f8d5a9cd688a85831
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2024-12-07 17:31:39 -0300

    fix: Pessoas constructor

--- entidade/negociante.py
+++ entidade/negociante.py
@@ -1,8 +1,8 @@
 from entidade.pessoa import Pessoa
 
 class Negociante(Pessoa):
-    def __init__(self, nome: str, documento: str, carros=None):
-        super().__init__(nome, documento, carros)
+    def __init__(self, nome: str, documento: str):
+        super().__init__(nome, documento)
 
     def tipo(self) -> str:
         return "Negociante"
